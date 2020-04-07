f277748214b03b331601f927b57d8aafa7ffaaad
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-07 22:57:47 +0530

    db migrate

--- books/models.py
+++ books/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Books(model.Model):
+class Books(models.Model):
     book_name = models.CharField(max_length=200)
     price = models.CharField(max_length=200)
     published_date = models.DateTimeField('date published')
 
-class Owners(model.Model):
-    book = models.ForiegnKey(Books, on_delete=models.CASCADE)
+class Owners(models.Model):
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
     owner_name = models.CharField(max_length=200)
     buy_date = models.DateTimeField('date bought')
