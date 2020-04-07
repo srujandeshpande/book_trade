4640e25a1345fdf8d65de8f94e046208d1e7edcf
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-07 22:50:11 +0530

    models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Books(model.Model):
+    book_name = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    published_date = models.DateTimeField('date published')
+
+class Owners(model.Model):
+    book = models.ForiegnKey(Books, on_delete=models.CASCADE)
+    owner_name = models.CharField(max_length=200)
+    buy_date = models.DateTimeField('date bought')
