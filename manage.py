101e2599f22dd4493b87191e5d99bcfe8cba6af7
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-22 00:19:36 +0530

    mosh1

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_trade.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyshop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pyshop/wsgi.py
+++ pyshop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for book_trade project.
+WSGI config for pyshop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_trade.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyshop.settings')
 
 application = get_wsgi_application()
